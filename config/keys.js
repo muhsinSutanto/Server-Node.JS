b19a807125c89510bb688e743e3a24011c6f1592
monggopesen 4 <sutantomuhsin@gmai.com>
2019-02-12 15:35:30 +0700

    routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://muhsin123:muhsin123@cluster1-shard-00-00-o2wap.mongodb.net:27017,cluster1-shard-00-01-o2wap.mongodb.net:27017,cluster1-shard-00-02-o2wap.mongodb.net:27017/test?ssl=true&replicaSet=Cluster1-shard-0&authSource=admin&retryWrites=true'
+}
