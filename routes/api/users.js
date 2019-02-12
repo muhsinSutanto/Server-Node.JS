157a727c0f5a8ac888d0ce600c8c35cb5322ff28
monggopesen 4 <sutantomuhsin@gmai.com>
2019-02-12 17:48:47 +0700

    add login

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,20 +8,28 @@ const User = require('../../models/User')
 //@route Get api/users/test
 //@desc Test users route
 //@access Public
-router.get('/test', (req, res)=> res.json({msg: 'users works'}));
+router.get('/test', (req, res) => res.json({
+    msg: 'users works'
+}));
 
 
 //@route Get api/users/test
 //@desc Test users route
 //@access Public
-router.post('/register',(req, res)=> {
-    User.findOne({ email: req.body.email})
+router.post('/register', (req, res) => {
+    User.findOne({
+            email: req.body.email
+        })
         .then(user => {
-            if(user) {
-                return res.status(400).json({email: 'Email already exist'})
-            }else{
+            if (user) {
+                return res.status(400).json({
+                    email: 'Email already exist'
+                })
+            } else {
                 const avatar = gravatar.url(req.body.email, {
-                    s:'200', r:'pg', d:'mm'
+                    s: '200',
+                    r: 'pg',
+                    d: 'mm'
                 })
 
                 const newUser = new User({
@@ -31,19 +39,54 @@ router.post('/register',(req, res)=> {
                     password: req.body.password
                 })
 
-                bycrypt.genSalt(10, (err, salt)=> {
+                bycrypt.genSalt(10, (err, salt) => {
                     bycrypt.hash(newUser.password, salt, (err, hash) => {
-                        if(err) throw err;
+                        if (err) throw err;
                         newUser.password = hash;
                         newUser
                             .save()
                             .then(user => res.json(user))
                             .catch(err => console.log(err))
-                    } )
+                    })
                 })
             }
         })
 })
+
+//@route Get api/users/test
+//@desc Test users route
+//@access Public
+router.post('/login', (req, res) => {
+    console.log(req)
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // find user by email
+    User.findOne({
+        email
+    }).then(user => {
+        // check for user
+        if (!user) {
+            return res.status(404).json({
+                email: 'user not found'
+            })
+        }
+
+        // check password
+        bycrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                res.json({
+                    msg: 'Success'
+                })
+            } else {
+                return res.status(400).json({
+                    password: 'password incorrect'
+                })
+            }
+        })
+    })
+})
+
 
 
 module.exports = router;
